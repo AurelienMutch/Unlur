ed826a7760a00895bf895844f3eb611517b89e79
Florent Bergere <bergeref@f203-04.e-im2ag.ujf-grenoble.fr>
2018-05-07 16:19:02 +0200

    test.java renomer en TestCase.java

---
+++ src/TestCase.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class TestCase {
+	public static void main(String[] args) {
+		Terrain t = new Terrain(3);
+		ArrayList<Case> oui = t.getVoisins(1,-1,0);
+		for (int i =0;i<oui.size();i++) {
+			System.out.println(oui.get(i).getX()+","+oui.get(i).getY()+","+oui.get(i).getZ());
+		}
+	}
+}
